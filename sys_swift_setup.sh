ea983417bc375b98d2ff2b10d17f3b8b3f97f1c0
Nandini Tata <nandini.tata@intel.com>
2016-10-07 17:26:46 +0000

    debugging memcache port

--- sys_swift_setup.sh
+++ sys_swift_setup.sh
@@ -165,7 +165,9 @@ EOF
    MEMCACHE_PORT=$(expr 11211 + ${CLUSTER_NUMBER})
    cp /etc/memcached.conf /etc/memcached_${SWIFT_USER}.conf
    cp ${SWIFT_REPO_DIR}/etc/memcache.conf-sample ${SWIFT_CONFIG_DIR}/memcache.conf
-   sed -i 's/^\(-p \)\([0-9]*\)/echo "\1$(('"${MEMCACHE_PORT}"'))"/ge' /etc/memcached_${SWIFT_USER}.conf
+   echo "************NT************"
+   echo "${MEMCACHE_PORT}"
+   sed -i 's/^\(-p \).*/echo "\1$(('"${MEMCACHE_PORT}"'))"/ge' /etc/memcached_${SWIFT_USER}.conf
    sed -i 's/^\(#\)\( memcache_servers =.*\)/echo "\2"/ge' ${SWIFT_CONFIG_DIR}/memcache.conf
    sed -i "s/11211/${MEMCACHE_PORT}/g" ${SWIFT_CONFIG_DIR}/memcache.conf
 

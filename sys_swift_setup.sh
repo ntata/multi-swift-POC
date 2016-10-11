f8c2f646247999690ea7f47bdedb01ca9a8b1778
Nandini Tata <nandini.tata@intel.com>
2016-10-11 17:42:32 +0000

    updated conditional stmts

--- sys_swift_setup.sh
+++ sys_swift_setup.sh
@@ -173,14 +173,14 @@ EOF
    #Here, we configure all the simulated storage nodes to log to one facility
    sed -i 's/^\(#\)\(local\.\*.*\)/\2/g' ${SWIFT_CONFIG_DIR}/swift-rsyslog.conf 
    sed -i "s/\/var\/log\/swift/\/var\/log\/${SWIFT_USER}/g" ${SWIFT_CONFIG_DIR}/swift-rsyslog.conf
-   if [${CLUSTER_COUNT} -eq 1] then
+   if [${CLUSTER_COUNT} -eq 1]; then
       sed -i 's/^\(log_facility = LOG_LOCAL\)\([0-9]\)/echo "\1$((1+'"${CLUSTER_COUNT}"'))"/ge' ${SWIFT_CONFIG_DIR}/account-server/*.conf
       sed -i 's/^\(log_facility = LOG_LOCAL\)\([0-9]\)/echo "\1$((1+'"${CLUSTER_COUNT}"'))"/ge' ${SWIFT_CONFIG_DIR}/container-server/*.conf
       sed -i 's/^\(log_facility = LOG_LOCAL\)\([0-9]\)/echo "\1$((1+'"${CLUSTER_COUNT}"'))"/ge' ${SWIFT_CONFIG_DIR}/object-server/*.conf
       sed -i 's/^\(log_facility = LOG_LOCAL\)\([0-9]\)/echo "\1$((1+'"${CLUSTER_COUNT}"'))"/ge' ${SWIFT_CONFIG_DIR}/object-server/*.conf
       sed -i 's/^\(log_facility = LOG_LOCAL\)\([0-9]\)/echo "\1$((2+'"${CLUSTER_COUNT}"'))"/ge' ${SWIFT_CONFIG_DIR}/object-expirer.conf
    fi
-   if [${CLUSTER_COUNT} -eq 2] then
+   if [${CLUSTER_COUNT} -eq 2]; then
       sed -i 's/^\(log_facility = LOG_LOCAL\)\([0-9]\)/echo "\1$((3+'"${CLUSTER_COUNT}"'))"/ge' ${SWIFT_CONFIG_DIR}/proxy-server.conf
       sed -i 's/^\(log_facility = LOG_LOCAL\)\([0-9]\)/echo "\1$((4+'"${CLUSTER_COUNT}"'))"/ge' ${SWIFT_CONFIG_DIR}/account-server/*.conf
       sed -i 's/^\(log_facility = LOG_LOCAL\)\([0-9]\)/echo "\1$((4+'"${CLUSTER_COUNT}"'))"/ge' ${SWIFT_CONFIG_DIR}/container-server/*.conf
